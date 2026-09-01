6389deb338f8357f60fc29de3ffc823f2e460e55
Kai Khoo <scottkhoo3@gmail.com>
2026-09-01 23:55:37 +1000

    added a hero, category filter bar and events row

--- src/main/java/com/eventsphere/app/LandingPageController.java
+++ src/main/java/com/eventsphere/app/LandingPageController.java
@@ -1,4 +1,23 @@
 package com.eventsphere.app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
 public class LandingPageController {
+    @FXML private ImageView heroImage;
+    @FXML private Label heroTitle;
+    @FXML private Label heroBlurb;
+    @FXML private HBox heroNav;
+
+    @FXML
+    protected void onPrevHero() {
+        System.out.println("prev");
+    }
+
+    @FXML
+    protected void onNextHero() {
+        System.out.println("next");
+    }
 }
