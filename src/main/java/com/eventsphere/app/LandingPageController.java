d548f424716b51121321305bfc41fddbb1d7d99d
Kai Khoo <scottkhoo3@gmail.com>
2026-09-04 15:51:18 +1000

    Updated landing page, Map tab feature in progress

--- src/main/java/com/eventsphere/app/LandingPageController.java
+++ src/main/java/com/eventsphere/app/LandingPageController.java
@@ -1,15 +1,68 @@
 package com.eventsphere.app;
 
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class LandingPageController {
+
     @FXML private ImageView heroImage;
     @FXML private Label heroTitle;
     @FXML private Label heroBlurb;
     @FXML private HBox heroNav;
+
+    @FXML private HBox drawer;
+    @FXML private Button drawerTab;
+    @FXML private VBox detailsPanel;
+    @FXML private StackPane mapPanel;
+
+    private double mapX;
+    private double closedX;
+    private int state = 0;   // 0 = closed, 1 = map, 2 = map + details
+
+    @FXML
+    public void initialize() {
+        drawer.widthProperty().addListener((obs, oldVal, newVal) -> {
+            mapX = detailsPanel.getWidth();
+            closedX = mapX + mapPanel.getWidth();
+            if (state == 0) {
+                drawer.setTranslateX(closedX);
+            }
+        });
+    }
+
+    private void slideTo(double x) {
+        TranslateTransition tt = new TranslateTransition(Duration.millis(280), drawer);
+        tt.setToX(x);
+        tt.setInterpolator(Interpolator.EASE_BOTH);
+        tt.play();
+    }
+
+    @FXML
+    protected void onToggleDrawer() {
+        if (state == 0) {
+            state = 1;
+            drawerTab.setText("›");
+            slideTo(mapX);
+        } else {
+            state = 0;
+            drawerTab.setText("‹");
+            slideTo(closedX);
+        }
+    }
+
+    public void showEventDetails() {
+        state = 2;
+        drawerTab.setText("›");
+        slideTo(0);
+    }
 
     @FXML
     protected void onPrevHero() {
