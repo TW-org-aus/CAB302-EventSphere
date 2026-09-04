c3a107f4fccdc51d45da688987a3a8a9844e5da7
Tom Wehl <tomwehl0@gmail.com>
2026-09-04 14:10:06 +1000

    Messages list, and individual messages pages created with stub data and console writes for button clicks, notifications page created and basic nav created in landing page

--- src/main/java/com/eventsphere/app/LandingPageController.java
+++ src/main/java/com/eventsphere/app/LandingPageController.java
@@ -1,4 +1,39 @@
 package com.eventsphere.app;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LandingPageController {
+    @FXML
+    protected void onProfileClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("profile-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root, 1280, 800));
+    }
+
+    @FXML
+    protected void onMessagesClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("messages-list-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root, 1280, 800));
+    }
+
+    @FXML
+    protected void onNotificationsClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("notifications-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root, 1280, 800));
+    }
+
+
 }
